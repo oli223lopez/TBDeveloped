03a1ae46aecced54d034b77b08b1ad80826f35d5
shanesharareh <shane.s.sharareh@gmail.com>
2020-12-28 15:32:25 -0800

    add reducers, and store

---
+++ frontend/src/reducers/session_errors_reducer.js
@@ -0,0 +1,15 @@
+import { RECEIVE_SESSION_ERRORS,RECEIVE_CURRENT_USER } from '../actions/sessions_action';
+
+const nullErrors = [];
+
+const SessionErrorsReducer = (state = nullErrors, action) =>{
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_SESSION_ERRORS:
+      return action.errors;
+    case RECEIVE_CURRENT_USER:
+      return state;
+    default:
+      return state;
+  }  
+}
---
+++ frontend/src/store/store.js
@@ -0,0 +1,11 @@
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import rootReducer from '../reducers/root_reducer';
+
+const configStore = (preloadedState = {}) => (
+    createStore(rootReducer, preloadedState, applyMiddleware(thunk,logger))
+);
+
+export default configStore
