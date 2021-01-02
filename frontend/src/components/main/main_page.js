1de3b08076091639b1c59afc434a70d315e14d5c
William Leung <w.leung.93@gmail.com>
2021-01-02 01:00:58 -0800

    little css for main

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import '../../assets/stylesheets/main.scss';
 
 
 class MainPage extends React.Component {
 
     render() {
         return (
-            <div>
-                <footer>
-                    Copyright &copy; 2020 TBDeveloped
+            <div className='main_container'>
+                <div className='main_content'>
+                main content
+                </div>
+                <div className="footer_top_container">
+                    <div className='footer_sent_container'>
+                        <div className="footer_sent">
+                            Choose a better way to work
+                        </div>
+                        <div className='freeTrial_container'>
+                            <Link to={'/signup'} className='footer_freeTrial'>TRY FOR FREE</Link>
+                        </div>
+                    </div>
+                </div>
+                <footer className='footer_container'>
+                    <div className='copyRight'>
+                        Copyright &copy; 2020 TBDeveloped
+                        Social
+
+                    </div>
                 </footer>
             </div>
         );
