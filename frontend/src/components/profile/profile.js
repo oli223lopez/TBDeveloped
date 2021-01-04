8262284d6c6ccfce72614884f775cd9b85101dd0
William Leung <w.leung.93@gmail.com>
2021-01-04 00:46:03 -0800

    more css

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -18,8 +18,7 @@ class Profile extends React.Component {
     
 
     render() {
-        console.log('one', this.props.profile_questions)
-
+        let amtOfPost = 0
         const profile_questions = () => {
 
             if(this.props.profile_questions.length > 0){
@@ -28,20 +27,27 @@ class Profile extends React.Component {
                         if(this.props.currentUser.questions.includes(question._id)){
                             // console.log('jjello', this.props.currentUser);
                             if(question.user._id === this.props.currentUser.id){
-                                console.log('bellow', (question.user._id));
+                                // console.log('bellow', (question.user._id));
+                                amtOfPost += 1;
                                 return(
-                                    <div key={id}>
-                                        <div className="created_post_title">Created Posts:</div>
-                                        
-                                        <label>Question:
-                                            <Link to={`/question/${question._id}`}>
-                                                {question.subject}
-                                            </Link>
-                                        </label>
-                                        
-                                        <div>CONTENT:
-                                            {question.content}
+                                    <div key={id} className='questions_topic'>
+                                        <div className='individual_case'>
+                                            <label>
+                                                <div className='sub_label'>Case Id:</div>
+                                                <Link to={`/question/${question._id}`}>
+                                                <div className='actual_info'>{question._id}</div>
+                                                </Link>
+                                            </label>
+                                            <label>
+                                                <div className='sub_label'>Subject:</div>
+                                                <div className='actual_info'>{question.subject}</div>
+                                            </label>
+                                            <label>
+                                                <div className='sub_label'>Case Closed: </div>
+                                                <div className='actual_info'>{`${question.solved}`}</div>
+                                            </label>
                                         </div>
+                                        
                                     </div>
                                 )
                             }
@@ -68,22 +74,26 @@ class Profile extends React.Component {
                 // console.log(this.props.profile_questions)
             }
         }
-        
+        console.log('currentUserId', this.props.currentUser.id)
         return(
             <div className='profile_container'>
-                <div className='profile_name'>{this.props.currentUser.username}'s Profile</div>
-
-                <div>
+                
+                <div className="created_post_info">
+                    <div className="created_post_title">Created Posts:</div>
                     {profile_questions()}
                 </div>
 
+                <div className="user_profile_info">
+                    <div className='profile_name'>{this.props.currentUser.username}'s Profile</div>
 
+                    <img alt="robots" src={`https://robohash.org/${this.props.currentUser.id}?100x100`} className='roboImgApi'/>
 
+                    <div className='profile_post_amt'>Number of Posts: {amtOfPost}</div>
+                    <div className='profile_reponse_amt'>Response to Posts: 0</div>
+                </div>
 
-                {/* <div>
-                    <CreateQuestionFormContainer />
-                </div> */}
             </div>
+            
         )
     }
 }
