883f63f11e460055f5447764042c825fc34f063a
tom-cheung <wai.tomcheung@gmail.com>
2021-01-21 14:12:22 -0800

    populate currentUser slice of state upon registration

--- frontend/src/reducers/user_reducer.js
+++ frontend/src/reducers/user_reducer.js
@@ -1,5 +1,5 @@
 import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
-import { RECEIVE_USER_LOGOUT } from '../actions/session_actions';
+import { RECEIVE_USER_LOGOUT, RECEIVE_USER_SIGN_IN } from '../actions/session_actions';
 
 const userReducer = (state = {}, action) => {
     Object.freeze(state);
@@ -9,6 +9,8 @@ const userReducer = (state = {}, action) => {
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
             return action.currentUser 
+        case RECEIVE_USER_SIGN_IN: 
+            return action.currentUser
         case RECEIVE_USER_LOGOUT: 
             return {}; 
         default:
