a0482deee52c1d406bd1d48a2167917fb78a8e4a
shanesharareh <shane.s.sharareh@gmail.com>
2021-01-21 16:35:58 -0800

    add resolve question css

--- frontend/src/components/resolved/resolved_show.js
+++ frontend/src/components/resolved/resolved_show.js
@@ -34,18 +34,20 @@ class ResolvedShow extends React.Component{
                     <div className="resolved_question_description">
                         <div className="resolved_question_header">
                           <img className = "resolved_question-show-author-image" alt="robots" src={`https://robohash.org/${resolved[0].user._id}?100x100`} />
-                          <h1 className="question-author"> posted by {resolved[0].user.username}</h1>
-                            <h2>{resolved[0].subject}</h2>
-                            <p className="resolved_date_posted" >Resolved on: {Date(resolved[0].createdAt)}</p>
+                          <h1 className="resolved-question-author"> {resolved[0].user.username}</h1>
+                            <h2 className="resolved-question-subject">{resolved[0].subject}</h2>
                         </div>
-                         <div className="question_body">
+                         <div className="resolved_question_body">
                             <p >{resolved[0].content}</p>
 
                          </div>
-                        <div className="question_footer">
-                          <div className="footer-status">
-                              <p>Tag: {resolved[0].tag}</p>
+                        <div className="resolved_question_footer">
+                            <div className="resolved-date-resolved">
+                            <p className="resolved_date_posted" >Resolved on: {Date(resolved[0].createdAt)}</p>
+                            <div className="check-mark">&#10003;</div>
                             </div>
+                            
+                              <p>Tag: {resolved[0].tag}</p>
                         </div>
                     </div>
                 </div>
